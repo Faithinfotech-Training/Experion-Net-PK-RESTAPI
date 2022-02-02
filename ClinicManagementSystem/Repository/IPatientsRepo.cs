b1fe393c822944291855544be681f7bbffab6fbe
Ashwin Madhu <ashwin.madhu@experionglobal.local>
2022-02-02 22:25:41 +0530

    Patient Repo and Controller

---
+++ ClinicManagementSystem/Repository/IPatientsRepo.cs
@@ -0,0 +1,19 @@
+﻿using ClinicManagementSystem.Models;
+using ClinicManagementSystem.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinicManagementSystem.Repository
+{
+    public interface IPatientsRepo
+    {
+
+        Task<int> AddPatient(Patient usr);
+
+        Task UpdatePatient(Patient usr);
+
+        Task<List<Patient>> GetAllPatient();
+    }
+}
